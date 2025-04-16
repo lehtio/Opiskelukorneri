72d1172a378bbea61b6ce7086af4f806d63cfca0
lehtio <laura.l.lehtio@gmail.com>
2025-04-16 22:16:48 +0200

    vaihtosivu

--- opintokorneri/src/pages/Vaihto.jsx
+++ opintokorneri/src/pages/Vaihto.jsx
@@ -5,6 +5,10 @@ const Vaihto = () => {
     <div>
       <h1>Vaihtojutut</h1>
       <p>Ai vaihtoonko oot menossa?</p>
+
+      <h2>Unspoken säädöt ja ongelmat joita voit vaihdossa kohdata</h2>
+      <h2>Unspoken voitot - syitä lähteä vaihtoon</h2>
+      <h2>Taktikoi vaihto-opinnot - saa etumatkaa tutkintoon ja hae monipuolisempaa akateemista osaamista</h2>
     </div>
   );
 };
