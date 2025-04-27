fc2185762c04c30ee130d8e2ae2c937073f4f720
lehtio <laura.l.lehtio@gmail.com>
2025-04-27 10:45:58 +0200

    etusivun taustan fixaus

--- opintokorneri/src/pages/Home.jsx
+++ opintokorneri/src/pages/Home.jsx
@@ -1,12 +1,17 @@
-// src/pages/Home.jsx
 import React from 'react';
 import '../styles/Studies.css';
 
 const Home = () => {
   return (
     <div className="home-container">
+      {/* Taustakuva oikealle */}
+      <div className="background-image"></div>
+
+      {/* Varsinainen sisältö */}
       <div className="content">
-        <h1 className="title">Tervetuloa oppimaan ja oivaltamaan <span className="highlight">Opiskelukorneriin.</span></h1>
+        <h1 className="title">
+          Tervetuloa oppimaan ja oivaltamaan <span className="highlight">Opiskelukorneriin.</span>
+        </h1>
         <p className="description">
           Sikäli mikäli päädyit jo tälle sivulle ja sulla on jotain kysymyksiä tai haasteita, 
           heitä viestiä telegrammissa / muissa someissa &lt;3
